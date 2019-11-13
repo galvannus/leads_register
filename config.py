3f09cbf6c88104c51012730e4bf9b3eb2859177c
Jorge Marcial <galvannus@gmail.com>
2019-11-13 11:01:43 -0700

    Avance landing page

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'mysql://jorge:123123@localhost/leads_register'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    GOOGLE_MAPS_API_KEY = 'AIzaSyAnTzEhULJBmTvmgbpDD8QJM60okEelkNc'
 
 config = {
     'development': DevelopmentConfig,
